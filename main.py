3ebb717a43f58fb4241b84d5509fd0b78dbfd7f7
EtaroX <95373414+EtaroX@users.noreply.github.com>
2023-01-19 22:40:51 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,101 @@
+import openpyxl
+import json
+import os
+from dataclasses import dataclass
+from dataclasses_json import dataclass_json
+
+
+@dataclass_json
+@dataclass
+class LangPyt:
+  Pytanie: str # Pytanie
+  OdpowiedzA: str # Odpowiedź A
+  OdpowiedzB: str # Odpowiedź B
+  OdpowiedzC: str # Odpowiedź C
+
+@dataclass_json
+@dataclass
+class Pytanie:
+  NazwaPytania: str # Nazwa pytania
+  NumerPytania: str # Numer pytania
+  PytaniePL: LangPyt # Pytanie
+  PytanieENG: LangPyt # Pytanie ENG
+  PytanieDE: LangPyt # Pytanie DE
+  PoprawnaOdp: str # Poprawna odp
+  Media: str # Media
+  ZakresStruktury: str # Zakres struktury
+  LiczbaPunktow: str # Liczba punktów
+  Kategorie: str # Kategorie
+  NazwaBloku: str # Nazwa bloku
+  ZrodloPytania: str # Źródło pytania
+  OCoChcemyZapytac: str # O co chcemy zapytać
+  JakiMaZwiazekZBezpieczenstwem: str # Jaki ma związek z bezpieczeństwem
+  Status: str # Status
+  Podmiot: str # Podmiot
+
+def removeNotFuckingImportantFiles(arrayofnames):
+  for name in arrayofnames:
+    try:
+      os.remove("./videos/" + name)
+      print("% s removed successfully" % name)
+    except OSError as error:
+      print(error)
+      print("File path can not be removed")
+
+def read_excel():
+  temp = {}
+  wb = openpyxl.load_workbook('Baza.xlsx')
+  sheet = wb['Treść pytania']
+  tmp = []
+  for row in sheet.iter_rows(min_row=2, max_col=33):
+    if row[0].value is None:
+      continue
+    pytanie = Pytanie(
+      NazwaPytania=row[0].value,
+      NumerPytania=row[1].value,
+      PytaniePL=LangPyt( 
+        Pytanie=row[2].value,
+        OdpowiedzA=row[3].value,
+        OdpowiedzB=row[4].value,
+        OdpowiedzC=row[5].value
+      ),
+      PytanieENG=LangPyt( 
+        Pytanie=row[6].value,
+        OdpowiedzA=row[7].value,
+        OdpowiedzB=row[8].value,
+        OdpowiedzC=row[9].value
+      ),
+      PytanieDE=LangPyt( 
+        Pytanie=row[10].value,
+        OdpowiedzA=row[11].value,
+        OdpowiedzB=row[12].value,
+        OdpowiedzC=row[13].value
+      ),
+      PoprawnaOdp=row[14].value,
+      Media=row[15].value,
+      ZakresStruktury=row[16].value,
+      LiczbaPunktow=row[17].value,
+      Kategorie=row[18].value,
+      NazwaBloku=row[19].value,
+      ZrodloPytania=row[20].value,
+      OCoChcemyZapytac=row[21].value,
+      JakiMaZwiazekZBezpieczenstwem=row[22].value,
+      Status=row[23].value,
+      Podmiot=row[24].value
+    )
+    temp[pytanie.NazwaPytania] = pytanie
+    for i in range(25, 33):
+      if row[i].value != "":
+        tmp.append(row[i].value)
+  removeNotFuckingImportantFiles(tmp)
+  return temp
+
+
+def main():
+  with open('data.json', 'w') as outfile:
+    json.dump(read_excel(), outfile, default=lambda o: o.__dict__, ensure_ascii=False)
+  print("Done")
+
+
+if __name__ == "__main__":
+  main()
---
+++ readme.md
@@ -0,0 +1,26 @@
+# Parser bazy
+Imo proste,
+Pobierasz bazę z strony rządowej
+
+Jej nazwę zmieniasz na Baza.xlsx
+
+Wszystkie filmy wrzucasz do folderu "videos"
+
+I odpalasz skrypt main.py
+
+(btw, skrypt zmienia exela do jsona)
+
+## Wymagania
+
+- Python 3.8
+- openpyxl
+- dataclass
+- dataclass_json
+
+## FAQ
+- "Mam x error, co zrobić?"
+  - Zainstaluj wymagane biblioteki
+  - Porównaj swoją bazę pytań z przykładową w folderze "example" (Jeśli jest inna, zgłoś problem na githubie)
+  - Zgłoś problem na githubie (razem z logiem błędu)
+- "Ej Bo MaSz LiTeRóWkĘ w PlIkU, tO oTwOrZę PuLl ReQuEsTa"
+  - SHUT THE FUCK UP. Jeśli chcesz zeby twój nick byl w liście contributorów, to zrób pull requesta z poprawką. Jeśli nie, to nie rób. Nie będę poprawiał literówek, które nie są błędami.
